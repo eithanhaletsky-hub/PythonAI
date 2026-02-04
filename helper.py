2833ba8d3c4402bfb470b8ba81fe86b5166cca66
[Eithan] <[eithan.haletsky@gamil.com]>
2026-02-04 19:08:41 +0200

    commit massage

--- helper.py
+++ helper.py
@@ -1,10 +1,21 @@
+from time import ctime
+
 from dotenv import load_dotenv
 import os
 import streamlit as st
 from google import genai
+from google.genai import types
+import time
 
-all_models = ["gemini-3.0-flash",
-              "gemini-2.5-flash",
+def current_time() -> str:
+    """
+    פונקציה שמחזירה מה התאירך והשעה
+    """
+    return time.ctime()
+
+
+
+all_models = ["gemini-2.5-flash",
               "gemini-2.0-flash",
               "gemini-2.5-flash-lite",
               "gemini-2.0-flash-lite",
@@ -13,17 +24,34 @@ all_models = ["gemini-3.0-flash",
 def createclient():
     st.session_state.client = genai.Client(api_key=LoadAPIkey())
 
-def sendMessage(text,history=[]):
+def sendMessage(text,system_prompt,history=[]):
     if 'client' not in st.session_state:
         createclient()
 
     for model in all_models:
         client = st.session_state.client
         try:
-            chat = client.chat.create(
-                model = model
+            chat = client.chats.create(
+                model = model,
+                history = history,
+                config = types.GenerateContentConfig(
+                    system_instruction = system_prompt
+                )
             )
-        except:
+            ai = chat.send_message(text)
+            print(ai.text)
+            return ai.text
+        except Exception as e:
+            error = str(e)
+            print(e)
+            if "429" in error:
+                st.info("שלחת יותר מדי הודעות נסה מחר פעם נוספת")
+                return
+            if "503" in error:
+                st.info(f"המודל עמוס נסה  מודל אחר")
+            else:
+              st.info("error" + error)
+              return
             print (f"{model} not working...")
 
 def LoadAPIkey():
@@ -34,3 +62,32 @@ def LoadAPIkey():
 def showmessage(sender,text):
     newmessage = st.chat_message(sender)
     newmessage. write(text)
+
+def save_to_history(project,sender,text):
+    if project not in st.session_state:
+        st.session_state[project] = {
+            "history": []
+        }
+    st.session_state[project]["history"].append(
+        {
+            "role": sender,
+            "parts": [{"text": text}]
+        }
+    )
+
+def newpage(project):
+        st.session_state[project] = {
+            "history": []
+        }
+
+
+
+
+
+
+
+
+
+
+
+
--- pages/homeworkBot.py
+++ pages/homeworkBot.py
@@ -1,3 +1,5 @@
+from multiprocessing.connection import answer_challenge
+
 import streamlit as st
 from helper import *
 
@@ -13,11 +15,46 @@ api_key = LoadAPIkey()
 
 showmessage("ai", "How can I help you?")
 
+if "homework" not in st.session_state:
+    newpage("homework")
+
+system_prompt = """
+    # תפקיד
+    אתה בוט שמסייע בשיעורי בית.
+    
+    # מטרה
+    לעזור לי להבין את החומר ולפתור תרגילים.
+    הסברים צריכים להיות ברורים, מסודרים ובגובה העיניים.
+    כוון אותי לחשיבה הנכונה ולפתרון, ולא רק לתשובה הסופית.
+    
+    # סגנון
+    ענה בצורה אנושית, רגועה וישירה.
+    אם מתאים – תן דוגמאות קצרות.
+    
+    # מגבלות
+    אם אינך יודע את התשובה – אמור זאת במפורש.
+    אל תנחש ואל תמציא מידע.
+"""
+
+st.session_state["homework"]["system_prompt"] = system_prompt
+
+history = st.session_state["homework"]["history"]
+for line in history:
+    sender = line["role"]
+    if sender == "model":
+        sender = "ai"
+
+    text = line["parts"][0]["text"]
+    showmessage(sender,text)
+
 user = st.chat_input("הודעה שלך...")
 if user:
     showmessage("user",user)
-
-
+    save_to_history("homework","user",user)
+    history = st.session_state["homework"]["history"]
+    answer = sendMessage(user,system_prompt,history)
+    showmessage("ai", answer)
+    save_to_history("homework", "model", answer)
 
 
 
