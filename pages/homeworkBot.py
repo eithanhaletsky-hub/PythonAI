f81e8e916df4968cff2c2198c8310ae70cb2ce72
[Eithan] <[eithan.haletsky@gamil.com]>
2026-02-25 19:07:19 +0200

    done

--- pages/homeworkBot.py
+++ pages/homeworkBot.py
@@ -1,150 +1,75 @@
-from multiprocessing.connection import answer_challenge
-
 import streamlit as st
-from helper import *
+from helper import * #תביא את כל הפונקציות מהקובץ המשותף
+import PIL.Image
 
 st.set_page_config(
-    page_title="בוט לשיעורי בית",
-    page_icon=""
+    page_title="בוט שיעורי בית",
+    page_icon="🤓"
 )
 
-st.title("בוט לשיעורי בית📗")
+st.title("בוט שיעורי בית")
 
-api_key = LoadAPIkey()
+api_key = loadAPIKey() #מביא את הפונקציה מהקובץ
 
+#הודעה ראשונה מהצ'אט
+showMessage("AI","היי אני כאן כדאי לעזור לך")
 
-showmessage("ai", "How can I help you?")
-
+#אם לא הגדרנו את הפרויקט - צור אותו בזיכרון
 if "homework" not in st.session_state:
-    newpage("homework")
+    newPage("homework")
 
+#פרומפט מיוחד שמגדיר לAI איך לעבוד
 system_prompt = """
-    # תפקיד
-    אתה בוט שמסייע בשיעורי בית.
-    
-    # מטרה
-    לעזור לי להבין את החומר ולפתור תרגילים.
-    הסברים צריכים להיות ברורים, מסודרים ובגובה העיניים.
-    כוון אותי לחשיבה הנכונה ולפתרון, ולא רק לתשובה הסופית.
-    
-    # סגנון
-    ענה בצורה אנושית, רגועה וישירה.
-    אם מתאים – תן דוגמאות קצרות.
-    
-    # מגבלות
-    אם אינך יודע את התשובה – אמור זאת במפורש.
-    אל תנחש ואל תמציא מידע.
+    #תפקיד
+    אתה בוט שיעורי בית
+
+    #משימה
+    המשימה שלך - לעזור לי בשיעורי בית
+    תסביר ברור
+    תכוון אותי לתשובה הנכונה
+
+    #מגבלות
+    אם אתה לא יודע - תחפש בגוגל
+   **אל תמציא תשובה**
+    ענה כמו בן אדם - בצורה אנושית
+
+    ** אם השתמשת בכלי (Tool) תכתוב את התוצאה **
+    **אנחנו בשנת 2026**
 """
 
+#לשמור בזיכרון
 st.session_state["homework"]["system_prompt"] = system_prompt
 
 history = st.session_state["homework"]["history"]
 for line in history:
     sender = line["role"]
-    if sender == "model":
-        sender = "ai"
+    if sender == "model": #ג'מיני מצפה לקבל model
+        sender = "ai" #streamlit מצפה לקבל AI
 
-    text = line["parts"][0]["text"]
-    showmessage(sender,text)
+    text = line["parts"][0]["text"] #פשוט מוציאים את הטקסט מהמבנה של ג'מיני
+    showMessage(sender,text)
 
-user = st.chat_input("הודעה שלך...")
-if user:
-    showmessage("user",user)
+#מקום להקליד
+user = st.chat_input("ההודעה שלך...")
+
+image_button = st.file_uploader("העלאת תמונה", type=["png","jpg","jpeg"])
+
+if user: #אם יש הודעה
+
+    showMessage("user",user)
+    #שולפים את ההיסטוריה
+
+    image = None
+
+    if image_button:
+        image = PIL.Image.open(image_button)
+
     save_to_history("homework","user",user)
     history = st.session_state["homework"]["history"]
-    answer = sendMessage(user,system_prompt,history)
-    showmessage("ai", answer)
-    save_to_history("homework", "model", answer)
+    answer = sendMessage(user,system_prompt,history, image) #לשלוח לAI את ההודעה
 
+    showMessage("ai",answer) #תראה את התשובה
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    save_to_history("homework","model",answer)
 
 
